993d996074d73c676d891e4663880601bac9dd74
Mighty531 <alienloveth1317@gmail.com>
2024-09-19 12:23:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '2349130216441'
+global.nomerowner = ["2349130216441"]
 
 // Apikey 
 
